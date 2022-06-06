5ad779f4c65b646ba1d8eea79057326b6502900d
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-06-06 14:39:32 -0300

    add sora and roboto fonts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "@fontsource/sora";
+import "@fontsource/roboto";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
