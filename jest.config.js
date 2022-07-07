b2885e5349e16a723c031dcb1b4994d48ae441de
Lucas Pedroso <lucasmendoncawf@gmail.com>
2022-07-06 22:36:29 -0300

    configuração de testes unitarios

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  moduleNameMapper: {
+    "\\.(css|less|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/mocks/image.mock.js",
+  }
+}
